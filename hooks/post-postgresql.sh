a4c31e8ff9370ac4af33b4a453cdb5a6d6fb3edb
Guy Brand <guy@guymas.ch>
2018-10-04 19:32:28 +0200

    Add default postgres user and docker

--- hooks/post-postgresql.sh
+++ hooks/post-postgresql.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# initialize the database and start the server (enable autostart on login as well)
-DB_FOLDER=`brew --prefix`/var/postgres
-if [[ ! -e $DB_FOLDER ]]; then
-    initdb $DB_FOLDER -E UTF-8 -U postgres --lc-collate en_US.UTF-8
-fi
 brew services start postgresql
+
+psql -d postgres -c 'CREATE USER postgres SUPERUSER;'
+psql -d postgres -c 'GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;'

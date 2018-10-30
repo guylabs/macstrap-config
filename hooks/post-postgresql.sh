2940f7da16f1168dc46473791ca3b88deafbdeda
Guy Brand <guy@guymas.ch>
2018-10-30 19:58:07 +0100

    Add sleep between starting Postgres and creating users/roles

--- hooks/post-postgresql.sh
+++ hooks/post-postgresql.sh
@@ -3,5 +3,7 @@ set -e
 
 brew services start postgresql
 
+sleep 5
+
 psql -d postgres -c 'CREATE USER postgres SUPERUSER;'
 psql -d postgres -c 'GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;'

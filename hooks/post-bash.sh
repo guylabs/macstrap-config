0434e300e7cfaf0eba01705735b06e3f70c3788e
Guy Brand <guy@guymas.ch>
2019-03-23 12:13:01 +0100

    Use sudo to change default shell

--- hooks/post-bash.sh
+++ hooks/post-bash.sh
@@ -3,5 +3,5 @@
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
+  sudo chsh -s /usr/local/bin/bash;
 fi;

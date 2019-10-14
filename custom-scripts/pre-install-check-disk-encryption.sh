2a813b72b720199a64ca2253870919cde6138d2a
Guy Brand <guy@guymas.ch>
2019-10-14 08:44:30 +0200

    Fix typo

--- custom-scripts/pre-install-check-disk-encryption.sh
+++ custom-scripts/pre-install-check-disk-encryption.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ ! -z "$CI" ]]; then
+if [[ -z "$CI" ]]; then
   if ! sudo fdesetup status | grep -q 'FileVault is On.'; then
     echo "Please enable FileVault disk encryption and rerun the macstrap install."
     exit 1

9092342b7098d03c441972104b372c4806e96dfb
Guy Brand <guy@guymas.ch>
2020-10-03 11:17:43 +0200

    Allow reinstallation of asdf plugins

--- hooks/post-asdf.sh
+++ hooks/post-asdf.sh
@@ -2,22 +2,22 @@
 set -e
 
 # Install ASDF Java plugin
-asdf plugin add java
+asdf plugin add java || true
 
 # Install Java 8
-asdf install java adoptopenjdk-8.0.265+1
+asdf install java adoptopenjdk-8.0.265+1 || true
 
 # Install Java 11 and set it globally as default
-asdf install java adoptopenjdk-11.0.8+10
-asdf global java adoptopenjdk-11.0.8+10
+asdf install java adoptopenjdk-11.0.8+10 || true
+asdf global java adoptopenjdk-11.0.8+10 || true
 
 # Install Java 15
-asdf install java adoptopenjdk-15.0.0+36 
+asdf install java adoptopenjdk-15.0.0+36 || true
 
 # Install ASDF NodeJS plugin
-asdf plugin add nodejs
+asdf plugin add nodejs || true
 ${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring
 
 # Install NodeJS 12.13.1 and set it globally as default
-asdf install nodejs 12.13.1
-asdf global nodejs 12.13.1
+asdf install nodejs 12.13.1 || true
+asdf global nodejs 12.13.1 || true

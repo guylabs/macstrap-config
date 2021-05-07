2d38c04dbfa20f47d81481311b34e8a962110949
Guy Brand <guy@guymas.ch>
2021-05-07 07:14:27 +0200

    Update

--- hooks/post-asdf.sh
+++ hooks/post-asdf.sh
@@ -17,13 +17,13 @@ asdf install java adoptopenjdk-8.0.282+8 || true
 asdf install java adoptopenjdk-11.0.10+9 || true
 asdf global java adoptopenjdk-11.0.10+9 || true
 
-# Install Java 15
-asdf install java adoptopenjdk-15.0.2+7 || true
+# Install Java 16
+asdf install java adoptopenjdk-16.0.0+36 || true
 
 # Install ASDF NodeJS plugin
 asdf plugin add nodejs || true
 ${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring
 
-# Install NodeJS 12.13.1 and set it globally as default
-asdf install nodejs 12.13.1 || true
-asdf global nodejs 12.13.1 || true
+# Install NodeJS 14.16.0 and set it globally as default
+asdf install nodejs 14.16.0 || true
+asdf global nodejs 14.16.0 || true

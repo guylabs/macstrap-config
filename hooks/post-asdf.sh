cb37fd3279f8a5ed06977bd56ded1f400a5e6105
Guy Brand <guy@guymas.ch>
2021-12-06 07:57:11 +0100

    More M1 updates

--- hooks/post-asdf.sh
+++ hooks/post-asdf.sh
@@ -7,21 +7,18 @@ brew install gpg
 # Install ASDF Java plugin
 asdf plugin add java || true
 
-# Install Java 7
-asdf install java zulu-7.48.0.11 || true
-
 # Install Java 8
-asdf install java temurin-8.0.302+8 || true
+asdf install java liberica-8u312+7 || true
 
 # Install Java 11 and set it globally as default
-asdf install java temurin-11.0.12+7 || true
-asdf global java temurin-11.0.12+7 || true
+asdf install java liberica-11.0.13+8 || true
+asdf global java liberica-11.0.13+8 || true
 
 # Install Java 16
-asdf install java temurin-16.0.2+7 || true
+asdf install java liberica-16.0.2+7 || true
 
 # Install Java 17
-asdf install java temurin-17.0.0+35 || true
+asdf install java liberica-17.0.1+12 || true
 
 # Install ASDF NodeJS plugin
 asdf plugin add nodejs || true
